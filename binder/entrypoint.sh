2ff1d72ca39bafc041264291c13e299bbf270975
zanto21 <felix18@uni-bremen.de>
2025-06-27 12:16:08 +0200

    update

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -11,12 +11,16 @@ roslaunch --wait rvizweb rvizweb.launch &
 MONGODB_URL=mongodb://127.0.0.1:27017
 mongod --fork --logpath ${HOME}/mongod.log
 
+export NEEMHUB_MONGO_PW=qEWRqc9UdN5TD7No7cjymUA8QEweNz
+export NEEMHUB_MONGO_DB=neems
+export NEEMHUB_MONGO_HOST='neem-3.informatik.uni-bremen.de'
+export NEEMHUB_MONGO_PORT=28015
+
 # Create a symbolic link to the folder neem_data
 ln -s /neem_data ${PWD}/neem_data
 
 # Launch Knowrob
-export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
-roslaunch --wait knowrob knowrob.launch &
+roslaunch --wait knowrob openease-test.launch &
 
 # The following line will allow the binderhub start Jupyterlab, should be at the end of the entrypoint.
 exec "$@"
